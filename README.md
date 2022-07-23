8cd1f7913c6907a1629b3a8178f287efd1b656a5
Isaac Dubuque <idubuque@umich.edu>
2022-07-23 18:52:59 -0400

    framework init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # chipTable
 This is an on-chain interim solution to ERS. It will provide a simple chip lookup table for vendors. 
+
+
+## .env
+PRIVATE_KEY="<your_private_key>"
+- Used for deploying
+ALCHEMY_API_KEY="<your_alchemy_api_key>"
+- Used for forking mainnet
+COINMARKETCAP_KEY="<your_coinmarketcap_key>"
+- Used for tracking gas prices
---
+++ hardhat.config.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const { PRIVATE_KEY, COINMARKETCAP_KEY, ALCHEMY_API_KEY } = process.env || null;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      /*
+      forking: {
+        url: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      }
+      */
+    },
+
+    maticTest: {
+      url: "https://rpc-mumbai.maticvigil.com", // mumbai testnet
+      accounts: [PRIVATE_KEY],
+      gasMultiplier: 2,
+    },
+
+    maticMain: {
+      url: "https://polygon-rpc.com",
+      accounts: [PRIVATE_KEY],
+      gasMultiplier: 2
+    },
+  },
+
+  solidity: {
+    compilers: 
+    [
+      {
+        version: "0.8.5"
+      },
+    ],
+
+    settings:
+    {
+      optimizer: 
+      {
+        enabled: true,
+        runs: 200
+      }
+    },
+  },
+
+  docgen: {},
+
+  gasReporter: {
+    currency: "USD",
+    token: "MATIC",
+    gasPriceApi: "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice",
+    coinmarketcap: COINMARKETCAP_KEY
+  }
+};
